09bbdf5d42306143eb31db3063c1b79cbded3514
Hirosaji <hiroya.kato@nex.nikkei.com>
2018-12-08 19:00:37 +0900

    add ga script

---
+++ js/ga.js
@@ -0,0 +1,10 @@
+!(function(){
+	"use strict";
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-92245361-5');
+
+}());
