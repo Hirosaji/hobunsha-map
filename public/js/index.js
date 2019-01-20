e19e9c36a92836493f0c3a23acb3323c95c866ff
Hirosaji <hiroya.kato@nex.nikkei.com>
2019-01-20 17:21:55 +0900

    update ga script

--- public/js/index.js
+++ public/js/index.js
@@ -199,6 +199,7 @@
                 selectTitle = d3.select(this).nodes()[0].options.animeTitle;
                 imgNum = d3.select(this).nodes()[0].options.imgNum;
                 updateAnimeDetailWindow(clickedPlace, selectTitle);
+                gaSend("click_marker_tracking", "clickMarker: " + selectTitle + " - " + clickedPlace);  // send ga
               });
               assetLayerGroup.addLayer(marker);
             })
@@ -338,6 +339,8 @@
           // Update control
           d3.select(".leaflet-top.leaflet-right").html("");
           L.control.layers(baseMaps).addTo(map);
+
+          gaSend("click_title_tracking", "changeSlide: " + selectTitle);  // send ga
         } else {
           firstLoad = false;
         }
