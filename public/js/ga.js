0fa9812efe986df347a07cdf6eaebd5044a4887c
Hirosaji <hiroya.kato@nex.nikkei.com>
2019-01-20 17:59:06 +0900

    revert ga

--- public/js/ga.js
+++ public/js/ga.js
@@ -1,20 +1,20 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+!(function(){
+	"use strict";
 
-ga('create', 'UA-92245361-5', 'auto');
-ga('send', 'pageview');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-92245361-5');
+}());
 
 function gaSend(category, label){
     var data = {
         hitType: 'event',
-        eventCategory: category,
-        eventAction: 'hobunsha-map',
-        eventLabel: label,
-        transport: 'beacon'
+        event_category: category,
+        event_label: label
     };
 
-    if (location.host === "hirosaji.github.io") ga('send', data);
+    if (location.host === "hirosaji.github.io") gtag('event', 'hobunsha-map', data);
     else console.log(data);
 }
